e6d62d94a80b144d466fd5654a0aa00e8285010b
Maciej Czyżowicz <czyzu01@gazeta.pl>
2014-08-14 22:14:20 +0200

    qcc added

--- Sarima examples.R
+++ Sarima examples.R
@@ -24,3 +24,22 @@ doladowania.sarima.monthly<-sarima(doladowania.ts,1,0,0,1,0,0,183)
 
 
 sarima.for(doladowania.ts,7,1,0,0,1,0,0,7)
+
+
+library(qcc)
+
+parse_date_time("2014-07-01", "%y-%m-%d")
+
+plot(window(doladowania.ts, end=parse_date_time("2014-07-01", "%y-%m-%d")))
+
+dane_bazowe<-window(doladowania.ts, end=parse_date_time("2014-07-01", "%y-%m-%d"))
+dane_testowe<-window(doladowania.ts, start=parse_date_time("2014-07-01", "%y-%m-%d"))
+
+qcc (data=dane_bazowe, 
+     newdata=dane_testowe,
+         type="xbar.one", add.stats=TRUE, title="Ilość numerów doładowujących", xlab="dzien")
+
+fit<-lm(doladowania$ILE_NUMEROW~doladowania$DZIEN)
+
+
+plot (fit)
